2d39eaadbd46c99a0ab56433ffced000c68cca7b
Sergey Boltyshov <boltyshovs@yandex.ru>
2022-10-16 14:44:53 +0300

    commit main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] arse) {
-        int[] salesPrice = {23, 45, 61, 39};
+        int[] salesPrice = {23, 45, 61, 39, 12};
         SalesManager salesManager = new SalesManager(salesPrice);
         System.out.println("Самая большая цена продажи " + salesManager.max());
     }
