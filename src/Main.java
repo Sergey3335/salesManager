463e2dadf9c355e503bedaff1e4847f4854c117a
Sergey Boltyshov <boltyshovs@yandex.ru>
2022-10-17 13:28:30 +0300

    add mediumSales

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         int[] salesPrice = {23, 45, 61, 39};
         SalesManager salesManager = new SalesManager(salesPrice);
         System.out.println("Самая большая цена продажи " + salesManager.max());
+        System.out.println("Cредняя цена продажи" + " " + (salesManager.mediumSale()));
     }
 }
